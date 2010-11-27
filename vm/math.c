b31a861520c00508227352004d2f559c79b32c24
Matthew Stickney <fiendishlinux@gmail.com>
2010-11-27 02:13:28 -0500

    Removed duplicate math code

--- vm/math.c
+++ vm/math.c
@@ -2,24 +2,16 @@
 #include "vm.h"
 #include "mem.h"
 #include "ops.h"
+#include "math.h"
 
-static int do_binop(struct proc *p, int addr, char op)
+int do_binop(char *a, char *b, char op)
 {
 	int i,j;
-	char temp[4];
-	
-	if (load(p, addr, temp) == -1) {
-		fprintf(stderr, "do_binop: load failed\n");
-		return -1;
-	}
-	i = word2int(p->r);
-	j = word2int(temp);
-	if (i == -1) {
-		fprintf(stderr, "do_binop: non-numeric word in memory\n");
-		return -1;
-	}
-	if (j == -1) {
-		fprintf(stderr, "do_binop: non-numeric word in register\n");
+
+	i = word2int(a);
+	j = word2int(b);
+	if (i == -1 || j == -1) {
+		fprintf(stderr, "do_binop: non-numeric operand\n");
 		return -1;
 	}
 	switch(op) {
@@ -44,156 +36,43 @@ static int do_binop(struct proc *p, int addr, char op)
 		i /= j;
 		break;
 	}
-	int2word(i, p->r);
+	return i;
+}
+
+static int do_register_op(struct proc *p, int addr, char op, char *opname)
+{
+	char temp[4];
+	int res;
+	
+	if (load(p, addr, temp) == -1) {
+		fprintf(stderr, "%s: load failed\n", opname);
+		return -1;
+	}
+	res = do_binop(p->r, temp, op);
+	if (res < 0) {
+		fprintf(stderr, "%s: do_binop failed\n", opname);
+		return -1;
+	}
+	int2word(res, p->r);
 	return 0;
 }
 
 int add(struct proc *p, int addr)
 {
-	if (do_binop(p, addr, '+') != 0) {
-		fprintf(stderr, "add: do_binop failed\n");
-		return -1;
-	}
-	return 0;
+	return do_register_op(p, addr, '+', "add");
 }
 
 int subtract(struct proc *p, int addr)
 {
-	if (do_binop(p, addr, '-') != 0) {
-		fprintf(stderr, "subtract: do_binop failed\n");
-		return -1;
-	}
-	return 0;
+	return do_register_op(p, addr, '-', "subtract");
 }
 
 int multiply(struct proc *p, int addr)
 {
-	if (do_binop(p, addr, '*') != 0) {
-		fprintf(stderr, "multiply: do_binup failed\n");
-		return -1;
-	}
-	return 0;
+	return do_register_op(p, addr, '*', "multiply");
 }
 
 int divide(struct proc *p, int addr)
 {
-	if (do_binop(p, addr, '/') != 0) {
-		fprintf(stderr, "divide: do_binup failed\n");
-		return -1;
-	}
-	return 0;
-}		
-
-#if 0
-static int add(struct proc *p, int addr)
-{
-	int i,j;
-	char temp[4];
-
-	if (load(p, addr, temp) == -1) {
-		fprintf(stderr, "add: load failed\n");
-		return -1;
-	}
-	i = word2int(temp);
-	j = word2int(p->r);
-	if (i == -1) {
-		fprintf(stderr, "add: non-numeric word in memory: ");
-		print_word(stderr, temp);
-		fprintf(stderr, "\n");
-		return -1;
-	}
-	if (j == -1) {
-		fprintf(stderr, "add: non-numeric word in register: ");
-		print_word(stderr, p->r);
-		fprintf(stderr, "\n");
-		return -1;
-	}
-	i += j;
-	int2word(i, p->r);
-	return 0;
+	return do_register_op(p, addr, '/', "divide");
 }
-
-static int subtract(struct proc *p, int addr)
-{
-	char temp[4];
-	int i,j;
-
-	if (load(p, addr, temp) == -1) {
-		fprintf(stderr, "subtract: load failed\n");
-		return -1;
-	}
-	
-	i = word2int(p->r);
-	j = word2int(temp);
-	if (i == -1) {
-		fprintf(stderr, "subtract: non-numeric word in register\n");
-		return -1;
-	}
-	if (j == -1) {
-		fprintf(stderr, "subtract: non-numeric word in memory\n");
-		return -1;
-	}
-	if (i < j) {
-		fprintf(stderr, "subtract: negative result\n");
-		return -1;
-	}
-	i -= j;
-	int2word(i, p->r);
-	return 0;
-}
-
-static int multiply(struct proc *p, int addr)
-{
-	char temp[4];
-	int i,j;
-
-	if (load(p, addr, temp) == -1) {
-		fprintf(stderr, "multiply: load failed\n");
-		return -1;
-	}
-
-	i = word2int(p->r);
-	j = word2int(temp);
-	if (i == -1) {
-		fprintf(stderr, "multiply: non-numeric word in register\n");
-		return -1;
-	}
-	if (j == -1) {
-		fprintf(stderr, "multiply: non-numeric word in memory\n");
-		return -1;
-	}
-
-	i *= j;
-	int2word(i, p->r);
-	return 0;
-}
-
-static int divide(struct proc *p, int addr)
-{
-	char temp[4];
-	int i,j;
-
-	if (load(p, addr, temp) == -1) {
-		fprintf(stderr, "divide: load failed\n");
-		return -1;
-	}
-
-	i = word2int(p->r);
-	j = word2int(temp);
-	if (i == -1) {
-		fprintf(stderr, "divide: non-numeric word in register\n");
-		return -1;
-	}
-	if (j == -1) {
-		fprintf(stderr, "divide: non-numeric word in memory\n");
-		return -1;
-	}
-	if (j == 0) {
-		fprintf(stderr, "divide: division by zero\n");
-		return -1;
-	}
-	i /= j;
-	int2word(i, p->r);
-	return 0;
-}
-#endif
---
+++ vm/math.h
@@ -0,0 +1,1 @@
+int do_binop(char *a, char *b, char op);
